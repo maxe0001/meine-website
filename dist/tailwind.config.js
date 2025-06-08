122de8c935a3ff8e9b81f02c7e557c89ae2113f0
maxe0001 <maxe.schumann@googlemail.com>
2025-06-08 23:22:34 +0200

    neu 08.06.

--- dist/tailwind.config.js
+++ dist/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: { extend: {} },
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
